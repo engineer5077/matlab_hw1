8ed73bd21413faacff758e3bfadfdbad592476f9
engineer5077 <albertmalizia132@gmail.com>
2024-09-27 15:49:26 +0000

    finished task 1

---
+++ homework_1/task_1.m
@@ -0,0 +1,22 @@
+% Author Name: [Albert Malizia]
+% Email: [malizi49@students.rowan.edu]
+% Course: MATLAB Programming - Fall 2024
+% Assignment: Assignment 2
+% Task: [task_1.m]
+% Date: [9/27/2024]
+
+% Variable Operations
+
+R = 5; % This variable stores the radius
+disp('The area is')
+A = (pi() * R^2) % This variable represents the area
+disp('The circumference is')
+C = (2 * pi() * R) % This variable represents the circumference
+
+fprintf("For a Circle the radius is, R")
+fprintf("For a Circle the area is, A")
+fprintf("For the circumference of a Circle is, C")
+
+% git add homework_1/task_1.m
+% git commit -m 'finished task 1'
+% git push original main
